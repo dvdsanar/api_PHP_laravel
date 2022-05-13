d4d50b6a6c81d663833a9a73b3ba8a007ae5bc0d
David S <dvdsanar@gmail.com>
2022-05-14 00:33:30 +0200

    feat: game routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -25,7 +26,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group([
     'middleware' => 'jwt.auth'
 ], function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
 });
 
@@ -38,4 +39,14 @@ Route::group([
     Route::post('/users', [UserController::class, 'createNewUser']);
     Route::put('/users/{id}', [UserController::class, 'updateUserById']);
     Route::delete('/users/{id}', [UserController::class, 'deleteUserById']);
+});
+
+Route::group([
+    'middleware' => 'jwt.auth'
+], function(){
+    Route::post('/game', [GameController::class, 'newGame']);
+    Route::get('/game', [GameController::class, 'getGames']);
+    Route::get('/game/{id}', [GameController::class, 'gameById']);
+    Route::patch('/game/{id}', [GameController::class, 'updateGame']);
+    Route::delete('/game/{id}', [GameController::class, 'deleteGame']);
 });
