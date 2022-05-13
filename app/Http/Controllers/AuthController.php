722b9f8bb2987d297fb5597bd99e47e3d4fad042
David S <dvdsanar@gmail.com>
2022-05-14 00:32:21 +0200

    fix: can use authentication, add line in user model and some changes in athController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -12,10 +13,13 @@ class AuthController extends Controller
 {
     public function register(Request $request){
     try {
+        Log::info('Register');
+
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:4',
+            'alias' => 'required|string|max:255',
         ]);
         if($validator->fails()){
             return response()->json($validator->errors()->toJson(),400);
@@ -25,14 +29,17 @@ class AuthController extends Controller
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'password' => bcrypt($request->password),
+            'alias' => $request->get('alias'),
         ]);
 
         $token = JWTAuth::fromUser($user);
-
+        
         return response()->json(compact('user','token'),201);
 
     } catch (\Throwable $th) {
-        return response()->json(['error' => 'Error de Registro de usuario'],201);
+        Log::error('Fail, can not register User -> ' . $th->getMessage());
+        
+        return response()->json(['error' => 'Error from User register'],201);
 
     }
     }
@@ -55,7 +62,7 @@ class AuthController extends Controller
             'token' => $jwt_token,
             ]);
         } catch (\Throwable $th) {
-            return response()->json(['error' => 'Error de Registro de usuario'],201);
+            return response()->json(['error' => 'Error wrong user or password'],201);
         }
     }
 
@@ -66,12 +73,15 @@ class AuthController extends Controller
         ]);
 
         try {
+            Log::info('Init Logout');
+
             JWTAuth::invalidate($request->token);
             return response()->json([
                 'success' => true,
                 'message' => 'User logged out successfully'
             ]);
         } catch (\Exception $exception) {
+        Log::error('failed to Login User->' . $exception->getMessage());
         return response()->json([
             'success' => false,
             'message' => 'Sorry, the user cannot be logged out'
